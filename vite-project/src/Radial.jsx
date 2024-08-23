e187c05896b2e15e89c67cd532087659fcc51671
tushardhawas <tushardhawasssd@gmail.com>
2024-08-23 07:43:41 +0530

    nearly ready

--- vite-project/src/Radial.jsx
+++ vite-project/src/Radial.jsx
@@ -15,12 +15,16 @@ function App() {
     const { left, top, width, height } = currentTarget.getBoundingClientRect();
     const x = clientX - left;
     const y = clientY - top;
+    const shadowX = (x - width / 2) / 10;
+    const shadowY = (y - height / 2) / 10;
 
-    const gradient = `radial-gradient(circle at ${x}px ${y}px, rgba(0, 0, 0, 0.5), #121212 400px)`;
+    const gradient = `radial-gradient(circle at ${x}px ${y}px, rgba(0, 0, 0, 0.65), #121212 400px)`;
 
     setGradientStyle({
       background: gradient,
       transition: "background 3s ease-out",
+      boxShadow: `${shadowX}px ${shadowY}px 50px rgba(0, 0, 0, 0.2)`, // dynamic shadow
+
     });
   };
 
